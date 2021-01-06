9f6d19d1c2a9b031e3d3e2d78e0f93ee0a0e73fb
lqs <527089599@qq.com>
2021-01-06 14:32:16 +0800

    初始化提交

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -0,0 +1,55 @@
+package com.xuecheng.manage_cms.controller;
+
+import com.xuecheng.api.cms.CmsPageControllerApi;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_cms.service.PageService;
+import com.xuecheng.manage_cms.service.impl.PageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cms/page")
+public class CmsPageController implements CmsPageControllerApi {
+    @Autowired
+    PageService pageService;
+
+    @Override
+    @GetMapping("/list/{page}/{size}")
+    public QueryResponseResult findList(@PathVariable("page") int page, @PathVariable("size") int size, QueryPageRequest queryPageRequest) {
+
+        QueryResponseResult queryResponseResult = pageService.findList(page, size, queryPageRequest);
+        return queryResponseResult;
+    }
+
+    @Override
+    @PostMapping("/add")
+    public CmsPageResult add(@RequestBody CmsPage cmsPage) {
+
+        CmsPageResult cmsPageResult = pageService.add(cmsPage);
+        return cmsPageResult;
+    }
+
+    @Override
+    @GetMapping("/get/{id}")
+    public CmsPage findById(@PathVariable("id") String id) {
+        return pageService.getById(id);
+    }
+
+    @Override
+    @PutMapping("/edit/{id}")
+    public CmsPageResult edit(@PathVariable("id") String id,@RequestBody CmsPage cmsPage) {
+        return pageService.edit(id,cmsPage);
+    }
+
+    @Override
+    @DeleteMapping("/del/{id}")
+    public ResponseResult delete(@PathVariable("id") String id) {
+        return pageService.delete(id);
+    }
+
+
+}
